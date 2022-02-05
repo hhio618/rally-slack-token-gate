9621b2c75acfd7c0a4f4b50aac680d5d924f436e
hh <hhio618@gmail.com>
2022-02-05 16:57:15 +0330

    disabled rally client

--- src/app.js
+++ src/app.js
@@ -14,22 +14,22 @@ const username = process.env.RALLY_IO_USERNAME;
 const password = process.env.RALLY_IO_PASSWORD;
 const callback_url = process.env.RALLY_APP_CALLBACK;
 
-const rallyClient = new RallyClient(username, password, data_api_base_url,rally_api_url, rally_v1_url, callback_url)
+// const rallyClient = new RallyClient(username, password, data_api_base_url,rally_api_url, rally_v1_url, callback_url)
 
 const app = new App({
     token: process.env.SLACK_TOKEN, //Find in the Oauth  & Permissions tab
     signingSecret: process.env.SIGNING_SECRET, // Find in Basic Information Tab
     socketMode:true,
     appToken: process.env.SOCKET_TOKEN, // Token from the App-level Token that we created
-    customRoutes: [
-      {
-        path: rallyClient.callback_path,
-        method: ['POST'],
-        handler: (req, res) => {
-          // TODO: 
-        },
-      },
-    ],
+    // customRoutes: [
+    //   {
+    //     path: rallyClient.callback_path,
+    //     method: ['POST'],
+    //     handler: (req, res) => {
+    //       // TODO: 
+    //     },
+    //   },
+    // ],
 });
 
 const bot = new WebClient(process.env.SLACK_TOKEN)
@@ -72,4 +72,5 @@ app.event("message", async ({ command, message, event, say }) => { // Replace he
 
 )
 
-app.start(process.env.PORT)
+
+app.start(process.env.PORT || 5000)
