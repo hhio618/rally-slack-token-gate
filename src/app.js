436ce957795f9279aa9f034d638b647b0f8acc86
hh <hhio618@gmail.com>
2022-02-05 16:40:20 +0330

    fix typo

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const rally_v1_url =  `${data_api_base_url}/v1`;
 const rally_api_url = `${data_api_base_url}/api`;
 const username = process.env.RALLY_IO_USERNAME;
 const password = process.env.RALLY_IO_PASSWORD;
-const callback_url = process.env.RALLY_APP_CALLBACK || `http://localhost:${port}/callback`;
+const callback_url = process.env.RALLY_APP_CALLBACK;
 
 const rallyClient = new RallyClient(username, password, data_api_base_url,rally_api_url, rally_v1_url, callback_url)
 
