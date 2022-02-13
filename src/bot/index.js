c460527ba7f95ab2d882135f358a206d68127227
hh <hhio618@gmail.com>
2022-02-13 19:24:21 +0330

    log slack user id

--- src/bot/index.js
+++ src/bot/index.js
@@ -36,12 +36,13 @@ const bot = new App({
 // Admin authorization middleware.
 async function adminOnly({ payload, client, context, next }) {
   const slackUserId = payload.user;
+  console.log(slackUserId);
   if (slackUserId != SLACK_ADMIN_USER) {
-        await client.chat.postEphemeral({
-          channel: payload.channel,
-          user: slackUserId,
-          text: `Sorry <@${slackUserId}>, you aren't authorized to do such admin tasks.`
-        });
+        await client.chat.postEphemeral(
+          channel= payload.channel,
+          user= slackUserId,
+          text= `Sorry <@${slackUserId}>, you aren't authorized to do such admin tasks.`
+        );
         return
   }
   
