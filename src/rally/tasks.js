07a4a90ca252a092d16ad14c18d4acc69d0cd92e
hh <hhio618@gmail.com>
2022-03-09 14:01:11 +0330

    small fix

--- src/rally/tasks.js
+++ src/rally/tasks.js
@@ -6,8 +6,7 @@ const {httpPost, simpleStringify} = require("./utils");
 async function register() {
     const username = rallyClient.username;
     const password = rallyClient.password;
-    const response = await httpPost(rallyClient.rally_v1_url + "/oauth/register", {username, password}, {headers: {"Content-Type" :"application/json"}});
-    console.log(`###### Response = ${simpleStringify(response)}`);
+    const response = await httpPost(rallyClient.rally_v1_url + "/oauth/register", {username, password});
     const status = response.status;
     console.log(`status = ${status}`);
     const data = response.data;
