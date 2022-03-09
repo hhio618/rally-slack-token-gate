ef61993c3e3378f671ae02f8f1d964d593801762
hh <hhio618@gmail.com>
2022-03-09 19:44:35 +0330

    typo fix

--- src/rally/rally.js
+++ src/rally/rally.js
@@ -57,7 +57,6 @@ class RallyClient {
     }
 
     async requestRallyAccountId(code) {
-        console.log(callback_path);
         if (!this.access_token) {
           throw ErrNotRegistered;
         }
